b0701ad02b26a670de682595c2536d0405fbbdbf
damiendesvent <damiendesvent@gmail.com>
2023-02-01 15:55:17 -0500

    modif find contours pour verif manuelle

--- Labo_2/src/find_contours.py
+++ Labo_2/src/find_contours.py
@@ -8,7 +8,7 @@ for i in os.listdir('./Labo_2/data') :
     for j in os.listdir('./Labo_2/data/' + str(i)) :
         index = 0
         for k in os.listdir('./Labo_2/data/' + str(i) + '/' + str(j)) :
-            if not (os.path.isfile('./Labo_2/output/predict/' + str(i) + '/' + str(j) + '/' + str(k)) or os.path.isfile('./Labo_2/output/error/' + str(i) + '/' + str(j) + '/' + str(k))) :
+            if not (os.path.isfile('./Labo_2/output/clean/' + str(i) + '/' + str(j) + '/' + str(k)) or os.path.isfile('./Labo_2/output/error/' + str(i) + '/' + str(j) + '/' + str(k))) :
                 index += 1
                 if index < 10000 :
                     image = cv2.imread('./Labo_2/data/' + str(i) + '/' + str(j) + '/' + str(k))
@@ -19,6 +19,7 @@ for i in os.listdir('./Labo_2/data') :
                     contours, hierarchy = cv2.findContours(edged, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
                     img_output = img_gray
+                    img_view = image.copy()
 
                     nb_contours = 0
                     x,y,w,h = 0,0,0,0
@@ -28,14 +29,22 @@ for i in os.listdir('./Labo_2/data') :
                         if area > 400 :
                             nb_contours += 1
                             x,y,w,h = cv2.boundingRect(c)
-                            #cv2.rectangle(img_output, (x, y), (x + w, y + h), (36, 255, 12), 2) crée une bbox verte
+                            cv2.rectangle(img_view, (x, y), (x + w, y + h), (36, 255, 12), 2) #crée une bbox verte
 
-                    os.makedirs('./Labo_2/output/predict/' + str(i) + '/' + str(j) + '/', exist_ok=True)
+                    os.makedirs('./Labo_2/output/clean/' + str(i) + '/' + str(j) + '/', exist_ok=True)
                     os.makedirs('./Labo_2/output/error/' + str(i) + '/' + str(j) + '/', exist_ok=True)
+
                     if (nb_contours == 1) :
-                        img_output = image[y:y+h,x:x+w]
+                        print(str(k))
+                        rect = cv2.selectROI(img=img_view, windowName=str(k))
+                        cv2.destroyAllWindows()
+                        if rect == (0,0,0,0) :
+                            img_output = image[y:y+h,x:x+w]
+                            
+                        else :
+                            img_output = image[int(rect[1]):int(rect[1]+rect[3]), int(rect[0]):int(rect[0]+rect[2])]
                         img_output = cv2.resize(img_output,dsize=(40,40), interpolation=cv2.INTER_AREA)
-                        cv2.imwrite('./Labo_2/output/predict/' + str(i) + '/' + str(j) + '/' + str(k), img_output)
+                        cv2.imwrite('./Labo_2/output/clean/' + str(i) + '/' + str(j) + '/' + str(k), img_output)
                         good_prediction += 1
                     else :
                         bad_prediction += 1
