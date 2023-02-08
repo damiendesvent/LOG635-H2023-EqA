8e8194b0a8447144386f11fa607529644f531375
St-Louis <victor.st-louis.1@ens.etsmtl.ca>
2023-02-08 14:57:51 -0500

    missing variation

--- Labo_2/src/generate_variations.py
+++ Labo_2/src/generate_variations.py
@@ -7,6 +7,7 @@ def generate_variations(image):
         ('_90', cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)),
         ('_180', cv2.rotate(image, cv2.ROTATE_180)),
         ('_270', cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)),
+        ('_flip', cv2.flip(image, 0)),
         ('_flip_90', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_CLOCKWISE)),
         ('_flip_180', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_180)),
         ('_flip_270', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_COUNTERCLOCKWISE)),
