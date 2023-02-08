74ecc8ea092c139befbcb9f5bc8604e207f99774
St-Louis <victor.st-louis.1@ens.etsmtl.ca>
2023-02-08 14:52:54 -0500

    generate_variations

---
+++ Labo_2/src/generate_variations.py
@@ -0,0 +1,13 @@
+import cv2
+
+# returns 8 tuples (name_prefix, image)
+def generate_variations(image):
+    return (
+        ('', image),
+        ('_90', cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE)),
+        ('_180', cv2.rotate(image, cv2.ROTATE_180)),
+        ('_270', cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE)),
+        ('_flip_90', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_CLOCKWISE)),
+        ('_flip_180', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_180)),
+        ('_flip_270', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_COUNTERCLOCKWISE)),
+    )
