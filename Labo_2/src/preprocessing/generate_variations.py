54a1bb3215e2d299038b21a4ed50734fb0cee489
damiendesvent <damiendesvent@gmail.com>
2023-03-15 08:39:57 -0400

    update bancale du main

--- Labo_2/src/preprocessing/generate_variations.py
+++ Labo_2/src/preprocessing/generate_variations.py
@@ -12,3 +12,15 @@ def generate_variations(image):
         ('_flip_180', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_180)),
         ('_flip_270', cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_COUNTERCLOCKWISE)),
     )
+
+def get_flat_variations(image):
+    return (
+        image,
+        cv2.rotate(image, cv2.ROTATE_90_CLOCKWISE),
+        cv2.rotate(image, cv2.ROTATE_180),
+        cv2.rotate(image, cv2.ROTATE_90_COUNTERCLOCKWISE),
+        cv2.flip(image, 0),
+        cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_CLOCKWISE),
+        cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_180),
+        cv2.rotate(cv2.flip(image, 0), cv2.ROTATE_90_COUNTERCLOCKWISE),
+    )
